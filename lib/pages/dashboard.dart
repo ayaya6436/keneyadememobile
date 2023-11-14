962d92c9c18e34ae312baedd403239e1a4b3a851
ayaya <ayaya6436@gmail.com>
2023-11-14 01:05:31 +0000

    splash

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,14 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
 
-class Dashboard extends StatelessWidget {
-  // const Dashboard({super.key});
+  
 
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+   List imageList = [
+    {"id": 1, "image_path": 'assets/images/prevention1.jpg'},
+    {"id": 2, "image_path": 'assets/images/slide1.png'},
+  ];
+
+    final CarouselController carouselController = CarouselController();
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Image.asset(
+                  "assets/images/logokeneya.png",
+                  width: 162,
+                  height: 42,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(8.0), // Ajuste la marge selon tes besoins
+            child: Stack(
+              children: [
+                InkWell(
+                  onTap: () {
+                    print(currentIndex);
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15), // Ajuste selon tes besoins
+                    child: CarouselSlider(
+                      items: imageList
+                          .map(
+                            (item) => Image.asset(
+                              item['image_path'],
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          )
+                          .toList(),
+                      carouselController: carouselController,
+                      options: CarouselOptions(
+                        scrollPhysics: const BouncingScrollPhysics(),
+                        autoPlay: true,
+                        aspectRatio: 2,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: imageList.asMap().entries.map((entry) {
+                      return GestureDetector(
+                        onTap: () => carouselController.animateToPage(entry.key),
+                        child: Container(
+                          width: currentIndex == entry.key ? 17 : 7,
+                          height: 7.0,
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 3.0,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: currentIndex == entry.key
+                                ? Colors.red
+                                : Colors.teal,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,51 +1,27 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:keneyadememobile/pages/dashboard.dart';
-import 'package:lottie/lottie.dart';
-class splash extends StatefulWidget {
 
+class splash extends StatelessWidget {
+  const splash({super.key});
 
-  @override
-  State<splash> createState() => _splashState();
-}
-
-class _splashState extends State<splash> with TickerProviderStateMixin{
-  late final AnimationController _controller;
-
-
-  @override
-  void initState(){
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _controller.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:[
-        Lottie.network("https://assets3.lottiefiles.com/packages/lf20_AMBEWz.json",
-        controller: _controller,
-        onLoaded: (compos){
-          _controller
-          ..duration = compos.duration
-          ..forward().then((value){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Dashboard())
-            );
-          });
-        }
-        ),
-        Center(
-        child: Text("Preventions-Traitements-Conseils")
-        )
-        ]
-      ),
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          Image.asset("assets/images/logokeneya.png"),
+          const Text ("Preventions-Senbilisations-Conseils",
+          style: TextStyle(fontSize: 21, fontWeight:FontWeight.bold),
+          )
+        ],
+      ), 
+      nextScreen: Dashboard(),
+      splashIconSize: 200,
+      duration:3000,
+      splashTransition: SplashTransition.sizeTransition,
+      animationDuration: const Duration(seconds: 1),
+      
     );
   }
 }
