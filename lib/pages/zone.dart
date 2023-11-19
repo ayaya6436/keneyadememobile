c2b8d0c38b0cc354757945580e9f8e72021eb48f
ayaya <ayaya6436@gmail.com>
2023-11-19 12:45:59 +0000

    ajout map

--- lib/pages/zone.dart
+++ lib/pages/zone.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+class ZoneModel {
+  final int id;
+  final String nom;
+  final String longitude;
+  final String latitude;
+
+  ZoneModel({
+    required this.id,
+    required this.nom,
+    required this.longitude,
+    required this.latitude,
+  });
+
+  factory ZoneModel.fromJson(Map<String, dynamic> json) {
+    return ZoneModel(
+      id: json['id'],
+      nom: json['nom'],
+      longitude: json['longitude'],
+      latitude: json['latitude'],
+    );
+  }
+}
 
 class Zone extends StatefulWidget {
   @override
@@ -10,11 +35,46 @@ class Zone extends StatefulWidget {
 
 class ZoneState extends State<Zone> {
   late GoogleMapController mapController;
+  List<Marker> markers = [];
 
-  final LatLng _center = const LatLng(-33.86, 151.20);
+  final LatLng _center = const LatLng(12.67171,-7.89541);
 
-  void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
+
+    try {
+      List<ZoneModel> zones = await fetchZones();
+      _addMarkers(zones);
+    } catch (e) {
+      // Gestion des erreurs (affichage d'un message d'erreur, par exemple)
+    }
+  }
+
+  Future<List<ZoneModel>> fetchZones() async {
+    final response = await http.get(Uri.parse("http://10.0.2.2:8080/keneya/zones"));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((zoneData) => ZoneModel.fromJson(zoneData)).toList();
+    } else {
+      throw Exception('Failed to load zones');
+    }
+  }
+
+  void _addMarkers(List<ZoneModel> zones) {
+    setState(() {
+      markers = zones.map((zone) {
+        return Marker(
+          markerId: MarkerId(zone.id.toString()),
+          position: LatLng(double.parse(zone.latitude), double.parse(zone.longitude)),
+          infoWindow: InfoWindow(
+            title: zone.nom,
+            // Ajoutez d'autres détails de description si nécessaire.
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        );
+      }).toList();
+    });
   }
 
   @override
@@ -34,6 +94,7 @@ class ZoneState extends State<Zone> {
           target: _center,
           zoom: 11.0,
         ),
+        markers: Set.from(markers),
       ),
     );
   }
