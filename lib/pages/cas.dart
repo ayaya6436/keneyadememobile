6f34ab1b2b242bd2c3dcd7b9d41dd319d88c82dd
ayaya <ayaya6436@gmail.com>
2023-11-21 17:16:19 +0000

    design cas

--- lib/pages/cas.dart
+++ lib/pages/cas.dart
@@ -1,18 +1,20 @@
+
+
 import 'package:flutter/material.dart';
 
-class Cas extends StatefulWidget{
+class Cas extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return CasState();
   }
-
 }
-class CasState extends State<Cas>{
+
+class CasState extends State<Cas> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
+      // backgroundColor: Colors.white
+
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -22,9 +24,69 @@ class CasState extends State<Cas>{
         ),
       ),
       body: Column(
-
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 0.0),
+                child: Image.asset(
+                  "assets/images/zoneDanger.png",
+                  width: 188,
+                  height: 169,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Signaler cas",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+            padding: EdgeInsets.all(60), // Adjusted padding value
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+            child: ElevatedButton(
+           style: ButtonStyle(
+             backgroundColor: MaterialStatePropertyAll<Color>(Colors.grey),
+               elevation: MaterialStateProperty.all<double>(0)
+           ),
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.camera_alt),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 18),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text("Signaler", style: TextStyle(fontSize: 18)),
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
